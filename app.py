2a586246595ac224821ca31db5ded8c3dcfc68d5
DragunDev <plarisanmarc@gmail.com>
2024-10-03 14:56:51 +0800

    Update app.py Add route for flash card editor page

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/flashcard-editor")
+def flashcard_editor():
+    return render_template('flashcard_editor.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
