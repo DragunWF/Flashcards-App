03a1b43f5db4757dba6661ed7da67200fc7100a7
DragunDev <plarisanmarc@gmail.com>
2024-10-03 14:34:54 +0800

    Update main.js Create base functionality

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+function onReady() {
+  const revealBtnTexts = {
+    default: "Reveal Answer",
+    toggled: "Display Defintion",
+  };
+  const revealBtn = document.getElementById("flashcardRevealBtn1");
+  const flashcardText = document.getElementById("flashcardText1");
+  revealBtn.addEventListener("click", () => {
+    if (revealBtn.textContent === revealBtnTexts.default) {
+      revealBtn.textContent = revealBtnTexts.toggled;
+      // TODO: Change in the future to be dynamic (Both Answer and Definition)
+      flashcardText.textContent = "Functions";
+    } else {
+      revealBtn.textContent = revealBtnTexts.default;
+      flashcardText.textContent =
+        "A reusable block of code that performs a specific task when called.";
+    }
+  });
+}
+
+onReady();
