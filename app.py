a6ef107d94ec7ab71906edcdb9b326725244d29e
DragunDev <plarisanmarc@gmail.com>
2024-10-03 16:44:29 +0800

    Update app.py - Update date format for logging

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class WebApp:
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s %(levelname)s %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
+            datefmt="%b-%d-%Y",
         )
 
         # Run the application
