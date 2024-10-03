bc099462b92738a479d9645feaddcf6b9d6704f0
DragunDev <plarisanmarc@gmail.com>
2024-10-03 19:31:55 +0800

    Create database_keys.py

---
+++ utils/database_keys.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Keys(Enum):
+    # Root
+    FLASHCARDS = "/flashcards"
+
+    # Flashcard Keys
+    ANSWER = "answer"
+    DEFINITION = "definition"
+    TOPIC = "topic"
