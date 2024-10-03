a81e72e158e74f5c5877724d57684f8194cd3f66
DragunDev <plarisanmarc@gmail.com>
2024-10-03 16:18:27 +0800

    Update index.html, app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
 
-@app.route("/flashcard-editor")
+@app.route("/flashcard-editor", methods=["GET", "POST"])
 def flashcard_editor():
+    if request.method == "POST":
+        pass
     return render_template('flashcard_editor.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
