72961d973d8d13db6ab3cbd2d444871b00f09d81
DragunDev <plarisanmarc@gmail.com>
2024-10-03 16:41:58 +0800

    Update app.py - Update structure and logging configuration

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
+import logging
 from flask import Flask, render_template, request
+
+from utils.database_helper import DatabaseHelper
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
-def index():
-    return render_template('index.html')
+class WebApp:
+    @app.route('/', methods=["GET"])
+    def index():
+        return render_template('index.html')
 
+    @app.route("/flashcard-editor", methods=["GET", "POST"])
+    def flashcard_editor():
+        if request.method == "POST":
+            pass
+        return render_template('flashcard_editor.html')
 
-@app.route("/flashcard-editor", methods=["GET", "POST"])
-def flashcard_editor():
-    if request.method == "POST":
-        pass
-    return render_template('flashcard_editor.html')
+    @staticmethod
+    def start():
+        # Logging Configuration
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s %(levelname)s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+        # Run the application
+        DatabaseHelper.start_database()
+        app.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    WebApp.start()
