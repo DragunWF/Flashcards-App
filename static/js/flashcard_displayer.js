da665951861ce6095a7a964f2a96108ad8417a68
DragunDev <plarisanmarc@gmail.com>
2024-10-03 21:24:31 +0800

    Update flashcard_displayer.js

--- static/js/flashcard_displayer.js
+++ static/js/flashcard_displayer.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Flashcard Components
   const createFlashcardsBtn = document.getElementById("createFlashcardsBtn");
+  const nextFlashcardBtn = document.getElementById("flashcardNextBtn");
+  const previousFlashcardBtn = document.getElementById("flashcardPreviousBtn");
+  const flashcardText = document.getElementById("flashcardText");
+  const revealBtn = document.getElementById("flashcardRevealBtn");
   const revealBtnTexts = {
     default: "Reveal Answer",
     toggled: "Display Defintion",
   };
-  const revealBtn = document.getElementById("flashcardRevealBtn");
-  const flashcardText = document.getElementById("flashcardText");
+
+  // Flashcards data
   const flashcards = []; // Each element of arr: { answer: "", definition: "" }
+  let flashcardIndex = 0;
 
   function fillFlashcards() {
     let flashcardCount = 0;
@@ -30,20 +36,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function changeFlashcardContent() {
+    console.log("call");
+    flashcardText.innerText = flashcards[flashcardIndex].definition;
+  }
+
+  // Event listeners
   revealBtn.addEventListener("click", () => {
     if (revealBtn.textContent === revealBtnTexts.default) {
       revealBtn.textContent = revealBtnTexts.toggled;
-      // TODO: Change in the future to be dynamic (Both Answer and Definition)
-      flashcardText.textContent = "Functions";
+      flashcardText.textContent = flashcards[flashcardIndex].answer;
     } else {
       revealBtn.textContent = revealBtnTexts.default;
-      flashcardText.textContent =
-        "A reusable block of code that performs a specific task when called.";
+      flashcardText.textContent = flashcards[flashcardIndex].definition;
     }
   });
 
   createFlashcardsBtn.addEventListener("click", () => {
     window.location.href = "/flashcard-editor";
+  });
+
+  nextFlashcardBtn.addEventListener("click", () => {
+    flashcardIndex++;
+    if (flashcardIndex >= flashcards.length) {
+      flashcardIndex = 0;
+    }
+    changeFlashcardContent();
+  });
+
+  previousFlashcardBtn.addEventListener("click", () => {
+    flashcardIndex--;
+    if (flashcardIndex < 0) {
+      flashcardIndex = flashcards.length - 1;
+    }
+    changeFlashcardContent();
   });
 
   fillFlashcards();
