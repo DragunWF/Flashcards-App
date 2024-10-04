1881dcaacbf4550f8bf93ed26edc4d0976c87242
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-04 16:13:04 +0800

    Create utils.py

---
+++ helpers/utils.py
@@ -0,0 +1,13 @@
+import random
+from string import ascii_letters, digits
+
+class Utils:
+    CODE_CHAR_COUNT_LIMIT = 6
+
+    @staticmethod
+    def generate_code() -> str:
+        char_pool = ascii_letters + digits
+        code = ""
+        for i in range(Utils.CODE_CHAR_COUNT_LIMIT):
+            code += char_pool[random.randint(0, len(char_pool) - 1)]
+        return code
