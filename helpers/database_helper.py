647dabe643fde0c9088fb7b8a8078157f12e9b54
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-04 17:40:36 +0800

    Update database_helper.py - Add functionality to create flashcard decks

--- helpers/database_helper.py
+++ helpers/database_helper.py
@@ -5,6 +5,7 @@ from firebase_admin import db
 
 from helpers.flashcard import Flashcard
 from helpers.database_keys import Keys
+from helpers.utils import Utils
 
 
 class DatabaseHelper:
@@ -32,3 +33,30 @@ class DatabaseHelper:
                                         data[key][Keys.DEFINITION.value]))
 
         return (data["topic"], flashcards)
+    
+    @staticmethod
+    def create_flashcard_deck(topic: str, flashcards: list[Flashcard]) -> str:
+        if not type(topic) is None or not type(flashcards) is list:
+            return None
+        ref = db.reference(Keys.FLASHCARDS.value)
+
+        # Validates newly generated codes to make sure a duplicate does not exist
+        new_code = Utils.generate_code()
+        while True:
+            has_duplicate = False
+            for code in ref.get():
+                if new_code == code:
+                    has_duplicate = True
+                    break
+            if has_duplicate:
+                new_code = Utils.generate_code()
+            else:
+                break
+
+        # Adds the flashcard data into the database
+        new_flashcard_deck = {"topic": topic}
+        for i, flashcard in enumerate(flashcards):
+            new_flashcard_deck[f"flashcard{i + 1}"] = flashcard.to_dict()
+        ref.child(new_code).set(new_flashcard_deck)
+
+        return new_code
