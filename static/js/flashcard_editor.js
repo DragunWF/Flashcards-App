b190aa394e5533c01fc40b7e69dcfe8edb330773
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-04 17:23:27 +0800

    Update flashcard_editor.js - Fix bug

--- static/js/flashcard_editor.js
+++ static/js/flashcard_editor.js
@@ -8,16 +8,19 @@ document.addEventListener("DOMContentLoaded", function () {
   addFlashcardBtn.addEventListener("click", () => {
     flashcardCount++;
     const newFlashcard = document.createElement("div");
+    const definitionId = `definition${flashcardCount}`;
+    const answerId = `answer${flashcardCount}`;
+
     newFlashcard.classList.add("flashcard", "mb-3");
     newFlashcard.innerHTML = `
         <h5 class="mb-3">Flashcard ${flashcardCount}</h5>
         <div class="mb-3">
-          <label for="definition${flashcardCount}" class="form-label">Definition</label>
-          <textarea class="form-control" id="definition${flashcardCount}" rows="2" placeholder="Enter the flashcard definition"></textarea>
+          <label for="${definitionId}" class="form-label">Definition</label>
+          <textarea name="${definitionId}" class="form-control" id="${definitionId}" rows="2" placeholder="Enter the flashcard definition"></textarea>
         </div>
         <div class="mb-3">
-          <label for="answer${flashcardCount}" class="form-label">Answer</label>
-          <input type="text" class="form-control" id="answer${flashcardCount}" placeholder="Enter the flashcard answer">
+          <label for="${answerId}" class="form-label">Answer</label>
+          <input name="${answerId}" "type="text" class="form-control" id="${answerId}" placeholder="Enter the flashcard answer">
         </div>
         <hr>
       `;
