96f48283cf1b978b93596debad1ca74d13274a3c
DragunDev <plarisanmarc@gmail.com>
2024-10-13 19:18:27 +0800

    Update reset_db.py - Fix bug and change credentials location

--- scripts/reset_db.py
+++ scripts/reset_db.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from dotenv import load_dotenv
 
 import firebase_admin
 from firebase_admin import credentials
@@ -8,8 +9,21 @@ from rich import print
 
 
 def start_database():
-    firebase_admin.initialize_app(credentials.Certificate('../private/credentials.json'), {
-        'databaseURL': "https://flashcards-app-58e80-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    load_dotenv()
+    firebase_admin.initialize_app(credentials.Certificate({
+        "type": os.getenv("TYPE"),
+        "project_id": os.getenv("PROJECT_ID"),
+        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+        "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "client_id": os.getenv("CLIENT_ID"),
+        "auth_uri": os.getenv("AUTH_URI"),
+        "token_uri": os.getenv("TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+        "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+    }), {
+        'databaseURL': os.getenv("DATABASE_URL")
     })
     print(f"Database has been started in {__file__}")
 
@@ -25,7 +39,7 @@ def reset_db():
     ref = db.reference(Keys.FLASHCARDS.value)
     flashcards = ref.get()
     for key in flashcards:
-        if key != 'TEST101':
+        if not key in Keys.TEST_DATA.values:
             ref.child(key).delete()
             print(f"Deleted flashcard deck: {key}")
     print("All user-created decks has been successfully deleted!")
